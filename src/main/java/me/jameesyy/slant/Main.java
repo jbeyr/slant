2f285651a4ca07efed1147f0e5d18427a096587e
jameesyy <jameson@jbey.dev>
2024-10-08 01:42:06 -0500

    bump to v1.2

--- src/main/java/me/jameesyy/slant/Main.java
+++ src/main/java/me/jameesyy/slant/Main.java
@@ -21,7 +21,7 @@ import org.lwjgl.input.Keyboard;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
     public static final String MODID = "slant";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     private static final Minecraft mc = Minecraft.getMinecraft();
     private static KeyBinding lmbAutoclickKey;
